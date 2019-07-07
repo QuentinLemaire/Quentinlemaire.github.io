0637eff7f8385b67ea961c91dd3e28c59015118b
Quentin <quentinlemr@gmail.com>
2019-07-07 20:02:53 +0200

    Portfolio

--- js/custom.js
+++ js/custom.js
@@ -104,8 +104,7 @@ $(document).ready(function () {
     });
 
     //animatedModal
-    $("#demo01").animatedModal();
-    $("#demo02").animatedModal();
+    $("#demo01,#demo02,#demo03,#demo04,#demo05,#demo06,#demo07,#demo08,#demo09").animatedModal();
 
    
 });
